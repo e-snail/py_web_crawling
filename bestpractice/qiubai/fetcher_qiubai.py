b363f92fcffb28f54b09e75f11dbfeed6161938f
Wu Yongbo <wilbur.k.woo@gmail.com>
2016-10-31 10:30:51 +0800

    解析作者信息

--- bestpractice/qiubai/fetcher_qiubai.py
+++ bestpractice/qiubai/fetcher_qiubai.py
@@ -8,7 +8,8 @@ import time
 
 
 # ----------- 加载处理糗事百科 -----------
-class Spider_Model:
+class SpiderModel:
+
     def __init__(self):
         self.page = 1
         self.pages = []
@@ -16,7 +17,7 @@ class Spider_Model:
 
         # 将所有的段子都扣出来，添加到列表中并且返回列表
 
-    def GetPage(self, page):
+    def get_page(self, page):
         myUrl = " http://www.qiushibaike.com/8hr/page/" + page
         #      myUrl =" http://www.qiushibaike.com/8hr/page/2"
         user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
@@ -27,15 +28,25 @@ class Spider_Model:
         # encode的作用是将unicode编码转换成其他编码的字符串
         # decode的作用是将其他编码的字符串转换成unicode编码
         unicodePage = myPage.decode("utf-8")
+
+        # joke_authors = re.findall('<a.*?href="/users.*?title=.*?>\n*<h2>(.*?)</h2>\n*</a>', unicodePage, re.S)
+        # authors = []
+        # for item in joke_authors:
+        #     authors.append(item)
+
         #    print(unicodePage)
         # 找出所有class="content"的div标记
         # re.S是任意匹配模式，也就是.可以匹配换行符
-        myItems = re.findall('<div.*?class="content".*?>\n*<span>(.*?)</span>\n*</div>', unicodePage, re.S)
+        joke_contents = re.findall('<div.*?class="content".*?>\n*<span>(.*?)</span>\n*</div>', unicodePage, re.S)
         items = []
-        for item in myItems:
+        index = 0
+        # print("authors size=" + len(authors) + "   joke_contents size=" + len(items))
+        for item in joke_contents:
             # item 中第一个是div的标题，也就是时间
             # item 中第二个是div的内容，也就是内容
-            items.append(item.replace("\n", ""))
+            # author_info = "作者:" + items[index]
+            # index += 1
+            items.append("  正文 " + item.replace("\n", ""))
         return items
 
         # 用于加载新的段子
@@ -47,7 +58,7 @@ class Spider_Model:
             if len(self.pages) < 2:
                 try:
                     # 获取新的页面中的段子们
-                    myPage = self.GetPage(str(self.page))
+                    myPage = self.get_page(str(self.page))
                     self.page += 1
                     self.pages.append(myPage)
                 except:
@@ -55,15 +66,15 @@ class Spider_Model:
             else:
                 time.sleep(1)
 
-    def ShowPage(self, nowPage, page):
-        for items in nowPage:
+    def show_page(self, current_page, page):
+        for items in current_page:
             print(u'第%d页' % page, items)
             myInput = input()
             if myInput == "quit":
                 self.enable = False
                 break
 
-    def Start(self):
+    def start(self):
         self.enable = True
         page = self.page
         print(page)
@@ -76,15 +87,12 @@ class Spider_Model:
         while self.enable:
             # 如果self的page数组中存有元素
             if self.pages:
-                nowPage = self.pages[0]
+                current_page = self.pages[0]
                 del self.pages[0]
-                self.ShowPage(nowPage, page)
+                self.show_page(current_page, page)
                 page += 1
 
-
-                # ----------- 程序的入口处 -----------
-
-
+# ----------- 程序的入口处 -----------
 print(u"""
 ---------------------------------------
    程序：糗百爬虫
@@ -99,5 +107,6 @@ print(u"""
 
 print(u'请按下回车浏览今日的糗百内容：')
 input(' ')
-myModel = Spider_Model()
-myModel.Start()
+myModel = SpiderModel()
+myModel.start()
+
